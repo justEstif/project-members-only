35966cc0f68d3e95cc57d02871d8d6a6f2a7300a
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-20 11:29:56 -0400

    Added luxon package; other files were styled

--- src/endpoints.config.ts
+++ src/endpoints.config.ts
@@ -1,12 +1,11 @@
-import * as dotenv from "dotenv"
-import path from "path"
+import * as dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({ path: path.resolve(__dirname, "..", ".env") })
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
 export default {
   MONGO_URL: process.env.MONGO_URL ?? "",
   PORT: process.env.PORT || 5000,
   UPDATE_PASSWORD: process.env.UPDATE_PASSWORD,
   DELETE_PASSWORD: process.env.DELETE_PASSWORD,
-}
-
+};
