2bcaec6b2c420f6fc069ab67da47b5fc9c7bbe07
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-20 17:49:52 -0400

    Added comments for how to sign up a user

--- src/routes/msgBoardRoutes.ts
+++ src/routes/msgBoardRoutes.ts
@@ -1,6 +1,28 @@
-import { Router } from "express";
+// import { Router } from "express";
 
-const routes = Router()
+// const routes = Router()
 
 // NOTE: Homepage
-// routes.get("/", user_controller.index)
+// routes.get("/", message_controller.index)
+
+// Setting up sign-up page
+// app.get("/sign-up", (_: Request, res: Response) => res.render("sign-up-form"));
+// app.post("/sign-up", (req, res, next) => {
+//   new User({
+//     username: req.body.username,
+//     password: req.body.password,
+//   }).save((err) => {
+//     if (err) return next(err);
+//     else res.redirect("/");
+//   });
+// });
+//
+// // Setting up login page
+// app.get("/log-in", (_: Request, res: Response) => res.render("log-in"));
+// app.post(
+//   "/log-in",
+//   passport.authenticate("local", {
+//     successRedirect: "/",
+//     failureRedirect: "/",
+//   })
+// );
