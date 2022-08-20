625b158ef8451de7cb73345f94b7acd88561828a
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-20 11:29:12 -0400

    Added console print db connection error

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,34 @@
-import express, { Express } from "express"
-import endpoints from "./endpoints.config"
-import path from "path"
-import favicon from "serve-favicon"
-import mongoose from "mongoose"
-import compression from "compression"
-import helmet from "helmet"
+import express, { Express } from "express";
+import endpoints from "./endpoints.config";
+import path from "path";
+import favicon from "serve-favicon";
+import { connect, connection } from "mongoose";
+import compression from "compression";
+import helmet from "helmet";
 
-const app: Express = express()
-const port = process.env.PORT || 5000
+const app: Express = express();
+const port = process.env.PORT || 5000;
 
-//Body Parser Middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(compression()) // compress all paths
+app.use(compression());
 
 app.use(
   helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
   })
-)
+);
 
-// view engine setup
-app.set("views", path.join(__dirname, "..", "views"))
-app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "..", "views"));
+app.set("view engine", "pug");
 
-// serve favicon
-app.use(favicon(path.join(__dirname, "..", "public", "favicon.ico")))
+app.use(favicon(path.join(__dirname, "..", "public", "favicon.ico")));
 
-mongoose.connect(endpoints.MONGO_URL)
+connect(endpoints.MONGO_URL);
+connection.on("error", console.error.bind(console, "mongo connection error"));
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
-})
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
