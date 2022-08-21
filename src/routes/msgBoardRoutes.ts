35a5779ab8cf849dd2bc2d0968ac6cb72b666c45
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-21 05:57:43 -0400

    Removed extra notes

--- src/routes/msgBoardRoutes.ts
+++ src/routes/msgBoardRoutes.ts
@@ -4,70 +4,11 @@ import * as user_controller from "../controller/userController"
 
 const router = Router()
 
-// NOTE: Homepage
-// routes.get("/", message_controller.index)
-
-// Setting up sign-up page
-// app.get("/sign-up", (_: Request, res: Response) => res.render("sign-up-form"));
-// app.post("/sign-up", (req, res, next) => {
-//   new User({
-//     username: req.body.username,
-//     password: req.body.password,
-//   }).save((err) => {
-//     if (err) return next(err);
-//     else res.redirect("/");
-//   });
-// });
-//
-// // Setting up login page
-// app.get("/log-in", (_: Request, res: Response) => res.render("log-in"));
-// app.post(
-//   "/log-in",
-//   passport.authenticate("local", {
-//     successRedirect: "/",
-//     failureRedirect: "/",
-//   })
-// );
-
-// app.get("/log-out", (req, res, next) => {
-//   req.logout((err) => {
-//     if (err) {
-//       return next(err);
-//     } else {
-//       res.redirect("/");
-//     }
-//   });
-// });
-//
-
-// NOTE: Only show if not logged in, else redirect to home(or do nothing)
-
-// to sign up as member
-router.get("/sign-up", user_controller.user_sign_up_get)
-router.get("/sign-up", user_controller.user_sign_up_post)
-
-// to login as a user
-router.get("/sign-in", user_controller.user_sign_in_get)
-router.get("/sign-in", user_controller.user_sign_in_post)
-
-// NOTE: Only show if logged in, else redirect to home(or do nothing)
-
-// to sign-out -> no post needed
-router.get("/sign-out", user_controller.user_sign_out_get)
-
-// to join club
-router.get("/join-club", user_controller.user_join_club_get)
-router.get("/join-club", user_controller.user_join_club_post)
-
-// NOTE: Only show if club member
-
-// to become admin
-router.get("/be-admin", user_controller.be_admin_get)
-router.post("/be-admin", user_controller.be_admin_post)
-
 // NOTE: Messages route
-// GET request for one message.
-router.get("/message/:id", message_controller.message_detail)
+
+router.get("/", message_controller.index) // Homepage
+
+router.get("/message/:id", message_controller.message_detail) // message detail page
 
 // Only the sender or an admin can delete a message
 router.get("/message/:id/delete", message_controller.message_delete_get)
@@ -76,3 +17,24 @@ router.post("/message/:id/delete", message_controller.message_delete_post)
 // Only the sender of the message can update the message
 router.get("/message:id/update", message_controller.message_update_get)
 router.post("/message:id/update", message_controller.message_update_post)
+
+// NOTE: User route
+
+// to sign-out (if signed in)
+router.get("/user:id/sign-out", user_controller.sign_out_get)
+
+// to join club (if signed in)
+router.get("/user:id/join-club", user_controller.join_club_get)
+router.get("/user:id/join-club", user_controller.join_club_post)
+
+// to become admin (if a club member)
+router.get("/user:id/be-admin", user_controller.be_admin_get)
+router.post("/user:id/be-admin", user_controller.be_admin_post)
+
+// to sign up as member (if not logged in)
+router.get("/sign-up", user_controller.sign_up_get)
+router.get("/sign-up", user_controller.sign_up_post)
+
+// to login as a user (if not logged in)
+router.get("/sign-in", user_controller.sign_in_get)
+router.get("/sign-in", user_controller.sign_in_post)
