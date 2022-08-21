8232e410d023ee6c3df16c3e65c0054bf4b5e79a
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-21 05:19:00 -0400

    Formatted file using prettier

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,23 +1,20 @@
-import { Schema, model } from "mongoose";
-import bcryptjs from "bcryptjs";
+import { Schema, model } from "mongoose"
+import bcryptjs from "bcryptjs"
 
 interface IUser {
-  first_name: string;
-  last_name: string;
-  email: string;
-  password: string;
-  membershipStatus: string;
-  url?: string;
-  admin: boolean;
-  comparePassword: (
-    password: string,
-    callback: (arg1: null | Error, arg2?: boolean) => void
-  ) => boolean;
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  membershipStatus: string
+  url?: string
+  admin: boolean
+  comparePassword: (password: string, callback: (arg1: null | Error, arg2?: boolean) => void) => boolean
 }
 
 const UserSchema = new Schema<IUser>({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   membershipStatus: {
@@ -26,44 +23,45 @@ const UserSchema = new Schema<IUser>({
     enum: ["User", "Member"],
     default: "User",
   },
-  admin: { type: Boolean, default: false }
-});
+  admin: { type: Boolean, default: false },
+})
 
-UserSchema.virtual("url").get(function() {
-  return "/user/" + this._id;
-});
+UserSchema.virtual("fullName").get(function () {
+  return this.firstName && this.lastName ? `${this.lastName}, ${this.firstName}` : ""
+})
 
-UserSchema.pre("save", function(next) {
-  const user = this;
-  if (this.isModified("password") || this.isNew) {
-    if (user.password === undefined) return next();
+UserSchema.virtual("url").get(function () {
+  return "/user/" + this._id
+})
+
+UserSchema.pre("save", function (next) {
+  const user = this
+  if (user.isModified("password") || user.isNew) {
+    if (user.password === undefined) return next()
     bcryptjs.genSalt(10, (err, salt) => {
-      if (err) console.log(err);
+      if (err) console.log(err)
       else {
-        bcryptjs.hash(user.password, salt, function(err, hash) {
-          if (err) console.log(err);
+        bcryptjs.hash(user.password, salt, function (err, hash) {
+          if (err) console.log(err)
           else {
-            user.password = hash;
-            next();
+            user.password = hash
+            next()
           }
-        });
+        })
       }
-    });
-  } else return next();
-});
+    })
+  } else return next()
+})
 
 UserSchema.methods = {
-  comparePassword: function(
-    password: string,
-    callback: (arg1: null | Error, arg2?: boolean) => void
-  ) {
-    bcryptjs.compare(password, this.password, function(error, isMatch) {
-      if (error) return callback(error);
-      else callback(null, isMatch);
-    });
+  comparePassword: function (password: string, callback: (arg1: null | Error, arg2?: boolean) => void) {
+    bcryptjs.compare(password, this.password, function (error, isMatch) {
+      if (error) return callback(error)
+      else callback(null, isMatch)
+    })
   },
-};
+}
 
-const User = model<IUser>("User", UserSchema);
-export { IUser };
-export default User;
+const User = model<IUser>("User", UserSchema)
+export { IUser }
+export default User
