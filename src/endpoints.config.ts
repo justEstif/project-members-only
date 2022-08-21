61ea66ec6b771b3270dbbcf289f13bf5ec83299d
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-21 05:19:51 -0400

    Added alternative value to stop error

--- src/endpoints.config.ts
+++ src/endpoints.config.ts
@@ -1,12 +1,12 @@
-import * as dotenv from "dotenv";
-import path from "path";
+import * as dotenv from "dotenv"
+import path from "path"
 
-dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") })
 
 export default {
   MONGO_URL: process.env.MONGO_URL ?? "",
   PORT: process.env.PORT || 5000,
   UPDATE_PASSWORD: process.env.UPDATE_PASSWORD,
   DELETE_PASSWORD: process.env.DELETE_PASSWORD,
-  SESSION_SECRET: process.env.SESSION_SECRET4,
-};
+  SESSION_SECRET: process.env.SESSION_SECRET4 || "secret",
+}
