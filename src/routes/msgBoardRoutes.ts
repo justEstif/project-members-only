3c992736a83f416780f21b6a025ac83c04d59401
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-21 14:38:28 -0400

    Fixed wrong get response to post

--- src/routes/msgBoardRoutes.ts
+++ src/routes/msgBoardRoutes.ts
@@ -25,7 +25,7 @@ router.get("/user:id/sign-out", user_controller.sign_out_get)
 
 // to join club (if signed in)
 router.get("/user:id/join-club", user_controller.join_club_get)
-router.get("/user:id/join-club", user_controller.join_club_post)
+router.post("/user:id/join-club", user_controller.join_club_post)
 
 // to become admin (if a club member)
 router.get("/user:id/be-admin", user_controller.be_admin_get)
@@ -33,8 +33,10 @@ router.post("/user:id/be-admin", user_controller.be_admin_post)
 
 // to sign up as member (if not logged in)
 router.get("/sign-up", user_controller.sign_up_get)
-router.get("/sign-up", user_controller.sign_up_post)
+router.post("/sign-up", user_controller.sign_up_post)
 
 // to login as a user (if not logged in)
 router.get("/sign-in", user_controller.sign_in_get)
-router.get("/sign-in", user_controller.sign_in_post)
+router.post("/sign-in", user_controller.sign_in_post)
+
+export default router
