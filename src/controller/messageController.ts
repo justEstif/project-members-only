9debfb806ee4757bdd14f20460b4959badb5ad61
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-21 05:40:59 -0400

    Added message routes to controller

--- src/controller/messageController.ts
+++ src/controller/messageController.ts
@@ -5,3 +5,23 @@ import { RequestHandler } from "express"
 export const index: RequestHandler = (_, res) => {
   res.render("messages_index")
 }
+
+export const message_detail: RequestHandler = (_, res) => {
+  res.render("message_detail")
+}
+
+export const message_update_get: RequestHandler = (_, res) => {
+  res.render("message_update")
+}
+
+export const message_update_post: RequestHandler = (_, res) => {
+  res.render("message_update")
+}
+
+export const message_delete_get: RequestHandler = (_, res) => {
+  res.render("message_delete")
+}
+
+export const message_delete_post: RequestHandler = (_, res) => {
+  res.render("message_delete")
+}
