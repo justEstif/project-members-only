1b05b18d37a710c0607b0ba236ef13e4019bb983
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-21 16:39:52 -0400

    Fixed sign up and sign in routes

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -5,7 +5,8 @@ import User from "../models/user"
 
 // NOTE: Display all messages with user, and sort decreasing
 export const index: RequestHandler = (_, res) => {
-  res.send("messages_index")
+  // res.send("messages_index")
+  res.send("NOT IMPLEMENTED: Author list")
 }
 
 export const sign_up_get: RequestHandler = (_, res) => {
@@ -14,32 +15,29 @@ export const sign_up_get: RequestHandler = (_, res) => {
 
 export const sign_up_post = [
   // Check if the email doesn't exist on the db
-  body("firstName").trim().escape().optional().isAlphanumeric(),
-  body("lastName").trim().escape().optional().isAlphanumeric(),
+  body("firstName").trim().escape().optional(),
+  body("lastName").trim().escape().optional(),
   body("email")
     .trim()
     .escape()
     .exists()
+    .withMessage("Email is required")
     .isEmail()
     .custom((value) => {
-      User.findOne({ email: value }).exec((err, emailExists) => {
-        if (err) {
-          throw err
-        } else if (emailExists) {
-          throw new Error("User with email exists")
-        } else {
-          return true
-        }
-      })
-    }),
-  body("password").trim().escape().exists().isLength({ min: 4 }),
-  body("confirmPassword").custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error("Passwords don't match")
-    } else {
-      return true
-    }
-  }),
+      User.findOne({ email: value }).exec((err, emailExists) => (emailExists || err ? false : true))
+    })
+    .withMessage("User with email already exists"),
+  body("password").trim().escape().exists().withMessage("Password is required").isLength({ min: 4 }),
+  body("confirmPassword")
+    .trim()
+    .escape()
+    .exists()
+    .withMessage("Passwords don't match")
+    .isLength({ min: 4 })
+    .custom((value, { req }) => {
+      return value !== req.body.password ? false : true
+    })
+    .withMessage("Passwords don't match"),
 
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req)
@@ -66,26 +64,27 @@ export const sign_up_post = [
 ]
 
 export const sign_in_get: RequestHandler = (_, res) => {
-  res.render("sign_in_form")
+  res.render("sign_in_form", { title: "Sign Up" })
 }
 
 export const sign_in_post = [
-  body("email").trim().escape().exists().isEmail(),
-  body("password").trim().escape().exists().isLength({ min: 4 }),
+  body("email").trim().escape().exists().withMessage("Email is required").isEmail(),
+  body("password").trim().escape().exists().withMessage("Password is required").isLength({ min: 4 }),
 
-  (req: Request, res: Response) => {
+  (req: Request, res: Response, _: NextFunction) => {
     const errors = validationResult(req)
     switch (!errors.isEmpty()) {
       case true:
         res.render("sign_in_form", {
-          title: "Sign In User ",
+          title: "Sign In",
           errors: errors.array(),
         })
         return
       default:
+        // passport.authenticate('local', { failureRedirect: '/', failureMessage: true }),
         passport.authenticate("local", {
           successRedirect: "/",
-          failureRedirect: "/sign_in",
+          failureRedirect: "/sign-in",
         })
     }
   },
