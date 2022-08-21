06b95860f0be7a01838192560603f3e2aff39b5d
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-21 05:41:08 -0400

    Added user routes to controller

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -4,3 +4,39 @@ import { RequestHandler } from "express"
 export const index: RequestHandler = (_, res) => {
   res.render("messages_index")
 }
+
+export const sign_in_get: RequestHandler = (_, res) => {
+  res.render("sign_in")
+}
+
+export const sign_in_post: RequestHandler = (_, res) => {
+  res.render("sign_in")
+}
+
+export const sign_up_get: RequestHandler = (_, res) => {
+  res.render("sign_up")
+}
+
+export const sign_up_post: RequestHandler = (_, res) => {
+  res.render("sign_up")
+}
+
+export const sign_out_get: RequestHandler = (_, res) => {
+  res.render("sign_out")
+}
+
+export const join_club_get: RequestHandler = (_, res) => {
+  res.render("join_club")
+}
+
+export const join_club_post: RequestHandler = (_, res) => {
+  res.render("join_club")
+}
+
+export const be_admin_get: RequestHandler = (_, res) => {
+  res.render("be_admin")
+}
+
+export const be_admin_post: RequestHandler = (_, res) => {
+  res.render("be_admin")
+}
