90acda3da81a06d86886f675f5f7973b569a9ccb
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-22 08:35:26 -0400

    Added comment for clarity

--- src/modules/session.config.ts
+++ src/modules/session.config.ts
@@ -1,6 +1,6 @@
 import endpoints from "../endpoints.config"
 export default {
   secret: endpoints.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
 }
