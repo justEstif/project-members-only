9e9f576147913b377807678c2a3eebae568488f3
Estifanos <estifanosbeyene21@gmail.com>
2022-10-09 15:54:54 -0400

    docs: add notes on the user routes

---
+++ server/src/routes/user.routes.ts
@@ -0,0 +1,31 @@
+/*
+* GET /user/:id -> go to the user page
+* POST /user -> register user
+* UPDATE /user/:id -> update user
+* DELETE /user/:id -> delete user
+*/
+
+/*
+* GET /user/:id
+** to see all the post made by that user
+** only the user, club members or admins can go to the user's page
+*** a user can't go to another user's page
+*/
+
+/*
+* UPDATE /user/:id
+** only the user can edit there page
+*/
+
+/*
+* POST /user
+** there needs to be a create user schema
+*/
+
+/*
+* DELETE /user/:id
+** only an admin or owner of id can delete the user
+** just add the user to the banned list??
+*/
+
+// test that only valid users are created
