f4a30df6f72518fb62c433906941041bc7fc3470
Estifanos <estifanosbeyene21@gmail.com>
2022-10-11 13:48:24 -0400

    feat: add schema for login

--- server/src/schema/authentication.schema.ts
+++ server/src/schema/authentication.schema.ts
@@ -24,3 +24,16 @@ export const registerSchema = object({
 });
 
 export type TRegisterSchema = TypeOf<typeof registerSchema>;
+
+export const loginSchema = object({
+  body: object({
+    email: string({
+      required_error: "Email is required",
+    }).email("Not a valid email"),
+    password: string({
+      required_error: "Password is required",
+    }).min(6, "Password too short - should be 6 chars minimum"),
+  }),
+});
+
+export type TLoginSchema = TypeOf<typeof loginSchema>;
