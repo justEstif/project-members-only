7939cb7870a53e808c877b696e393b4b68d8f27c
Estifanos <estifanosbeyene21@gmail.com>
2022-10-24 13:39:15 -0400

    feat: remove required error messages

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -10,21 +10,15 @@ import { object, string, TypeOf } from "zod";
  */
 export const updateSchema = object({
   body: object({
-    name: string({
-      required_error: "Name is required",
-    }),
-    userName: string({
-      required_error: "Username is required",
-    }),
-    email: string({
-      required_error: "Email is required",
-    }).email("Not a valid email"),
-    password: string({
-      required_error: "Password is required",
-    }).min(6, "Password too short - should be 6 chars minimum"),
-    passwordConfirmation: string({
-      required_error: "passwordConfirmation is required",
-    }),
+    name: string({}),
+    userName: string({}),
+    email: string({}).email("Not a valid email"),
+    password: string({}).min(
+      6,
+      "Password too short - should be 6 chars minimum"
+    ),
+    passwordConfirmation: string({}),
+    secretKey: string({}),
   })
     .partial()
     .refine((data) => data.password && data.passwordConfirmation, {
